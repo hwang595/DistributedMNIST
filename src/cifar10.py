119381e8cd29d35424b2d7cc62fec97a45cc0757
Hongyi Wang <hongyiwang@cs.wisc.edu>
2018-06-20 21:39:04 -0500

    add func 33

--- src/cifar10.py
+++ src/cifar10.py
@@ -396,6 +396,8 @@ def maybe_download_and_extract():
     print()
     statinfo = os.stat(filepath)
     print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
-  
-  print('filepath', filepath)
-  tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+  extracted_dir_path = os.path.join(dest_directory, 'cifar-10-batches-bin')
+  if not os.path.exists(extracted_dir_path):
+    tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+  #print('filepath', filepath)
+  #tarfile.open(filepath, 'r:gz').extractall(dest_directory)
