b29dc12f3c0b9b68922af5c6ef998f4818767c17
Hongyi Wang <hongyiwang@cs.wisc.edu>
2018-06-20 21:04:54 -0500

    add func 32

--- src/cifar10_distributed_train.py
+++ src/cifar10_distributed_train.py
@@ -30,15 +30,13 @@ def main(unused_args):
       job_name=FLAGS.job_name,
       task_index=FLAGS.task_id)
 
-  cifar10.maybe_download_and_extract()
-
   if FLAGS.job_name == 'ps':
     # `ps` jobs wait for incoming connections from the workers.
     server.join()
   else:
     n_workers = len(worker_hosts)
     worker_id = int(FLAGS.task_id)
-
+    cifar10.maybe_download_and_extract()
     # Only the chief checks for or creates train_dir.
     if FLAGS.task_id == 0:
       if not tf.gfile.Exists(FLAGS.train_dir):
